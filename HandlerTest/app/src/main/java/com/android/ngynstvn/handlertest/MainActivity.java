bfd995ea58db29050d43d2cae854c3a26d79e716
Steven Nguyen <ngynstvn@gmail.com>
2015-11-13 23:16:39 -0800

    Studied how Handlers worked and have a good understanding as to how it relays messages containing data from one thread to another.

--- HandlerTest/app/src/main/java/com/android/ngynstvn/handlertest/MainActivity.java
+++ HandlerTest/app/src/main/java/com/android/ngynstvn/handlertest/MainActivity.java
@@ -1,52 +1,107 @@
 package com.android.ngynstvn.handlertest;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ProgressBar progressBar;
+    private TextView progressValue;
+    private TextView progressMax;
+
+    private Thread thread;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.tb_activity_main);
         setSupportActionBar(toolbar);
+        progressBar = (ProgressBar) findViewById(R.id.pb_activity_main);
+        progressValue = (TextView) findViewById(R.id.tv_progress_value);
+        progressMax = (TextView) findViewById(R.id.tv_progress_max_value);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        progressMax.setText(String.valueOf(progressBar.getMax()));
+
+        thread = new Thread(new MyThread());
+        thread.start();
+
+        // Instantiate Handler here. Because you instantiate it here in MainActivity, it is now
+        // with the MessageQueue of the Main (UI) thread
+
+        /**
+         *
+         * Once the message has been created from teh Thread that wants to send a task over
+         * to the Handler to perform the task by handling the message, the following is done:
+         *
+         */
+
+        handler = new Handler() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void handleMessage(Message msg) {
+                // The code here is going to run on this thread, the Main(UI) thread!
+                Log.v("(MainActivity)", "Thread in handleMessage(): " + Thread.currentThread().getName());
+                progressBar.setProgress(msg.arg1);
+                progressValue.setText(String.valueOf(msg.arg1));
             }
-        });
+        };
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    class MyThread implements Runnable {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // This method runs in the background
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public void run() {
+
+            /**
+             *
+             * Each time this loop is iterated, I want to tell the view (must be in
+             * UI thread, to update the value of the progress. How would I do that?
+             * Use a handler that will handle the task in the UI thread.
+             * In order to do that, one must first create a Message object. From there, the Handler will send that message
+             * to the thread it was instantiated in and perform the certain desired task.
+             * In this case, the Handler object is going to send the message to the UI thread
+             *
+             * Note: The Handler object does both sending message AND handling the message
+             *
+             */
+
+            for(int i = 0; i <= 100; i++) {
+
+                Log.v("(MainActivity)", "Thread in run(): " + Thread.currentThread().getName());
+
+                // Start creating the Message object here
+                // NOTE: If I created this line below outside the for loop, it will crash the application
+                // because the message object can only sends one message. Another Message needs to send
+                // another message and so on. The Message object cannot be constantly reassigned like
+                // that for delivery of the message.
+                //
+                Message message = Message.obtain();
+
+                // I want to send the value of i over to the UI thread. I need to assign the message
+                // argument to i. Then let the handler send it out
+                message.arg1 = i;
+                handler.sendMessage(message);
+
+                // Let the Thread sleep for 100 milliseconds otherwise the progress bar will finish
+                // instantly
+
+                try {
+                    Thread.sleep(50);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
