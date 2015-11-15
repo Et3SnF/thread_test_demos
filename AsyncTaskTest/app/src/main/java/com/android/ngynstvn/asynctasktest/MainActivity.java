ea2f4644bacb4f2cbda22c35575ddaab2f31f178
Steven Nguyen <ngynstvn@gmail.com>
2015-11-14 17:01:00 -0800

    Added stuff for AsyncTask. Couldn't find some good example on it...

--- AsyncTaskTest/app/src/main/java/com/android/ngynstvn/asynctasktest/MainActivity.java
+++ AsyncTaskTest/app/src/main/java/com/android/ngynstvn/asynctasktest/MainActivity.java
@@ -1,13 +1,52 @@
 package com.android.ngynstvn.asynctasktest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btn_test_thread);
+
+        Log.v("(MainActivity)", "Current thread in onCreate(): " + Thread.currentThread().getName());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("(MainActivity)", "Current thread in onClick(): " + Thread.currentThread().getName());
+            }
+        });
+    }
+
+    class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+        }
     }
 }
